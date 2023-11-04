137326d076d909eb01afae992980d48f56f6e097
Daniel Albarral Nunez <daniel@potion.ai>
2023-11-03 23:05:47 -0600

    remove bazel

--- 2023/CHALLENGE_01/python/main.py
+++ 2023/CHALLENGE_01/python/main.py
@@ -1,13 +1,25 @@
+"""
+Challenge 01: Message
+"""
+
 import pathlib
 from collections import defaultdict
 
+import colored
+
 
 def read_input(input_file: pathlib.Path):
+    """
+    Read input file
+    """
     with input_file.open("r") as f:
         return f.read()
 
 
 def main():
+    """
+    Decrypt message and print it
+    """
     input_file = pathlib.Path(pathlib.Path(__file__).parent.parent / "inputs/message_01.txt")
     message_txt = read_input(input_file)
     message = message_txt.split(" ")
@@ -16,6 +28,7 @@ def main():
     for word in lower_message:
         words_counter[word] += 1
     result = "".join(f"{w}{c}" for w, c in words_counter.items())
+    print(f"{colored.fg(2)}{colored.attr('bold')}Challenge 01:{colored.attr('reset')}:")
     print(result)
 
 if __name__ == "__main__":
--- README.md
+++ README.md
@@ -5,3 +5,19 @@
 # :technologist: [Codember Solutions](https://codember.dev)
 
 </div>
+
+
+## Installation instructions:
+```bash
+rtx install
+poetry install
+poetry run pip-compile --output-file=./third_parties/python/requirements_lock.txt third_parties/python/requirements.txt
+poetry run pip install -r third_parties/python/requirements_lock.txt
+```
+
+## 2023
+### [Challenge 1](./2023/CHALLENGE_01/%20CHALLENGE_01.md): 
+Python version:
+```
+bazel run //2023/CHALLENGE_01/python/...
+```
